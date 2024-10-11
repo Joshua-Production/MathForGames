973cdf04327cb60c04ca889aab51e639606c1a2d
Joshua-Production <joshuaespinosabusiness@outlook.com>
2024-10-11 10:26:39 -0500

    added vector 2 stuff

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -10,8 +10,8 @@ namespace Sandbox
         
         static void Main(string[] args)
         {
-            //Raylib.InitWindow(800, 480, "Hello World");
-            Raylib.InitWindow(Raylib.GetScreenWidth(), Raylib.GetScreenHeight(), "Hello World");
+            Raylib.InitWindow(800, 480, "Hello World");
+            //Raylib.InitWindow(Raylib.GetScreenWidth(), Raylib.GetScreenHeight(), "Hello World");
             Vector2 deltaPosition = new Vector2(0, 0);
             Vector2 position = new Vector2();
 
@@ -21,28 +21,24 @@ namespace Sandbox
                 int speed = 1500;
                 Raylib.BeginDrawing();
                 Raylib.ClearBackground(Color.Black);
-                Raylib.SetTargetFPS(60);
-                Raylib.DrawRectangle((int)position.X, (int)position.Y, 30, 30, Color.Violet);
-               
-                
-                Rectangle player = new Rectangle(0, 0, 0, 0);
-                
-                int up = Raylib.IsKeyDown(KeyboardKey.W);
-                int down = Raylib.IsKeyDown(KeyboardKey.S);
-                int left = Raylib.IsKeyDown(KeyboardKey.A);
-                int right = Raylib.IsKeyDown(KeyboardKey.D);
-
-                deltaPosition.Y -= up;
-                deltaPosition.Y += down;
-                deltaPosition.X -= left;
-                deltaPosition.X += right;
-
-                position.X = deltaPosition.X * Raylib.GetFrameTime() * speed;
-                position.Y = deltaPosition.Y * Raylib.GetFrameTime() * speed;
+                //Raylib.SetTargetFPS(60);
+                //Raylib.DrawRectangle((int)position.X, (int)position.Y, 30, 30, Color.Violet);
 
 
+                //Rectangle player = new Rectangle(0, 0, 0, 0);
 
-                
+                //int up = Raylib.IsKeyDown(KeyboardKey.W);
+                //int down = Raylib.IsKeyDown(KeyboardKey.S);
+                //int left = Raylib.IsKeyDown(KeyboardKey.A);
+                //int right = Raylib.IsKeyDown(KeyboardKey.D);
+
+                //deltaPosition.Y -= up;
+                //deltaPosition.Y += down;
+                //deltaPosition.X -= left;
+                //deltaPosition.X += right;
+
+                //position.X = deltaPosition.X * Raylib.GetFrameTime() * speed;
+                //position.Y = deltaPosition.Y * Raylib.GetFrameTime() * speed;
 
 
 
@@ -51,29 +47,33 @@ namespace Sandbox
 
 
 
-                // Raylib.DrawCircle(400, 250, 300, Color.Orange);
 
 
-                // //2
-                // Raylib.DrawEllipse(320, 200, 40, 250, Color.Yellow);
 
-                // //3
+
+                Raylib.DrawCircle(400, 250, 300, Color.Orange);
+
+
+                ////2
+                //Raylib.DrawEllipse(320, 200, 40, 250, Color.Yellow);
+
+                ////3
                 //Raylib.DrawEllipse(480, 200, 40, 220, Color.Yellow);
 
-                // // 1
-                // Raylib.DrawEllipse(210, 250, 40, 220, Color.Yellow);
+                //// 1
+                //Raylib.DrawEllipse(210, 250, 40, 220, Color.Yellow);
 
-                // //4
-                // Raylib.DrawEllipse(600, 250, 40, 220, Color.Yellow);
+                ////4
+                //Raylib.DrawEllipse(600, 250, 40, 220, Color.Yellow);
 
-                // Raylib.DrawEllipse(400, 320, 220, 280, Color.Maroon);
-                // Raylib.DrawEllipse(400, 320, 210, 250, Color.Red);
+                Raylib.DrawEllipse(400, 320, 220, 280, Color.Maroon);
+                Raylib.DrawEllipse(400, 320, 210, 250, Color.Red);
 
-                // Raylib.DrawEllipse(400, 200, 140, 90, Color.Maroon);
+                Raylib.DrawEllipse(400, 200, 140, 90, Color.Maroon);
 
-                // Raylib.DrawEllipse(400, 200, 130, 70, Color.SkyBlue);
-                // Raylib.DrawEllipse(340, 170, 40, 17, Color.RayWhite);
-                // Raylib.DrawText("SHHHHHH!", 280, 380, 50, Color.White);
+                Raylib.DrawEllipse(400, 200, 130, 70, Color.SkyBlue);
+                Raylib.DrawEllipse(340, 170, 40, 17, Color.RayWhite);
+                Raylib.DrawText("SHHHHHH!", 280, 380, 50, Color.White);
 
 
                 Raylib.EndDrawing();
